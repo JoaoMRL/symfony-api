f67ace19e75073a786b10f11d8b22197c38205bc
JoaoMRL <ribeiroleite.joaomarco@gmail.com>
2023-06-26 14:27:33 +0200

    Ajout du persist en POST

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -1,8 +1,11 @@
 <?php 
 
 namespace App\Controller;
+use App\Entity\Movie;
 use App\Repository\MovieRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('/api/movie')]
 class MovieController extends AbstractController{
@@ -26,5 +29,13 @@ class MovieController extends AbstractController{
         }
         return $this->json(null,204);
         
+    }
+    #[Route(methods:'POST')]
+    public function insert (Request $request){
+        $formData =$request->toArray();
+    //  return $this->json($formData);
+        $film = new Movie($formData['title'],$formData['resume'],new DateTime($formData['released']),$formData['duree']);
+        $this->movie->persist($film);
+        return $this->json($film,201);
     }
 }
