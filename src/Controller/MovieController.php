b02ccb83eecc60b0f1b7db418ebf6337ad905e35
JoaoMRL <ribeiroleite.joaomarco@gmail.com>
2023-06-26 14:00:59 +0200

    Completion de MovieController

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -4,14 +4,14 @@ namespace App\Controller;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/api/movie')]
 class MovieController extends AbstractController{
     public function __construct( private MovieRepository $movie){}
-    #[Route('/api/movie', methods:'GET')]
+    #[Route(methods:'GET')]
     public function index (){
         return $this->json($this->movie->findAll());
     }
-    #[Route('/api/movie/{id}', methods:'GET')]
+    #[Route('/{id}', methods:'GET')]
     public function indexId (int $id){
         if ($this->movie->findById($id) !== null){
             return $this->json($this->movie->findById($id));
@@ -19,12 +19,12 @@ class MovieController extends AbstractController{
         return $this->json('Resource Not Found',404);
         
     }
-    #[Route('/api/movie/{id}', methods:'DELETE')]
+    #[Route('/{id}', methods:'DELETE')]
     public function indexIdDelete (int $id){
         if ($this->movie->findById($id) !== null){
             return $this->json($this->movie->delete($id));
         }
-        return $this->json('Resource Not Found',404);
+        return $this->json(null,204);
         
     }
 }
